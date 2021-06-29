12aadeb01905e739c3dd483e5c3280f446fe2da4
MissterMan <putrayogi4545@gmail.com>
2021-06-29 19:46:44 +0700

    Versi 1.0, safe version!!!

---
+++ Documents/NetBeansProjects/UASpbo1/src/uaspbo1/FXMLDocumentController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uaspbo1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+
+/**
+ *
+ * @author Prayogi
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    private Label label;
+    @FXML
+    private BorderPane borderPane;
+    @FXML
+    private Button sideInputBtn;
+    @FXML
+    private Button sideDataBtn;
+    @FXML
+    private Button sideExitBtn;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void OnClickHandleButtonActionInput(MouseEvent event) {
+        sideInputBtn.setStyle("-fx-background-color:#FD9941");
+        sideDataBtn.setStyle("-fx-background-color:#FC6A03");
+        sideExitBtn.setStyle("-fx-background-color:#FC6A03");
+    }
+
+    @FXML
+    private void onClickHandleButtonActionData(MouseEvent event) {
+        sideInputBtn.setStyle("-fx-background-color:#FD9941");
+        sideDataBtn.setStyle("-fx-background-color:#FC6A03");
+        sideExitBtn.setStyle("-fx-background-color:#FC6A03");
+    }
+    
+}
